51a3a180732be23006726f84b6eaed50db9a594a
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-07-23 14:10:57 +0530

    scirpt update

--- script.js
+++ script.js
@@ -8,8 +8,7 @@ async function getData(cityName) {
     const response = await fetch(
         `http://api.weatherapi.com/v1/current.json?key=214f28f38b8441c9aa923049240605&q=${cityName}&aqi=yes`
     );
-    const data = response.json();
-    // console.log(data)
+    const data = await response.json();
     return data;
 }
 
